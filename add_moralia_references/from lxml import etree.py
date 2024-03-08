a5e8de99be32f86cac6f559ca8efbe31bdbade8f
Jack Noutch <jack.noutch@gmail.com>
2024-03-08 19:13:46 +0000

    Update of gitignore and deletion of temporary files

--- add_moralia_references/from lxml import etree.py
+++
@@ -1,9 +0,0 @@
-from lxml import etree
-
-xml = "<bibl>text1<author>text2</author>tail1<id>text3<bibl>text4</bibl>tail2</id>tail3</bibl>tail4"
-
-root = etree.fromstring(xml)
-
-traversed_elements = [[e.text, e.tail] for e in root]
-
-print(traversed_elements)
--- add_moralia_references/regex_test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-regex = r"(a|b)"
-
-text = "123abc456"
-
-splits = re.split(regex, text)
-
-print(splits)
